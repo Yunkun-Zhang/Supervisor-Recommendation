64c9c99cc935b6e7360d39a5c65e58e3d1517bd6
Caplimbo <zhhqu0305@gmail.com>
2021-05-31 14:59:39 +0800

    add sampling and splitting

--- meta_path.py
+++ meta_path.py
@@ -1,11 +1,19 @@
+"""
+Definition and Acquirement of metapaths in sr-MAGNN
+
+Node types:
+P: Paper - 0
+A: Author - 1
+F: Fields - 2, c for child, p for parent
+
+edges: A-P, A-F, P-F, Fc-Fp
+
+Metapath types: FcFpFc, FpFcFp, APA, AFA, PAP, PFP, PFAFP, AFPFA
+"""
+import numpy as np
 from process_data import *
-
-"""
-0 for paper
-1 for author
-2 for field   
-"""
-
+import math
+from split_and_sample import *
 
 def find_meta_path(data):
     num_author, num_paper, num_field = len(data['authors']), len(data['papers']), len(data['fields'])
@@ -17,7 +25,35 @@ def find_meta_path(data):
     type_mask[num_paper:num_paper + num_author] = 1  # 1 for author
     type_mask[num_paper + num_author:] = 2  # 2 for field
 
+    # dataset split
+    neg_af = get_neg_samples(data['author_field'], num_author, num_field)
+    neg_pf = get_neg_samples(data['paper_field'], num_paper, num_field)
+    af_pos_train, af_pos_val, af_pos_test = get_train_val_test_split(data['author_field'], 0.1, 0.1)
+    pf_pos_train, pf_pos_val, pf_pos_test = get_train_val_test_split(data['paper_field'], 0.1, 0.1)
+    af_neg_train, af_neg_val, af_neg_test = get_train_val_test_split(neg_af, 0.1, 0.1)
+    pf_neg_train, pf_neg_val, pf_neg_test = get_train_val_test_split(neg_pf, 0.1, 0.1)
+
+    np.savez('../data/preprocessed/train_val_test_neg_author_field.npz',
+             train_neg_author_field=af_neg_train,
+             val_neg_author_field=af_neg_val,
+             test_neg_author_field=af_neg_test)
+    np.savez('../data/preprocessed/train_val_test_pos_author_field.npz',
+             train_pos_author_field=af_pos_train,
+             val_pos_author_field=af_pos_val,
+             test_pos_author_field=af_pos_test)
+    np.savez('../data/preprocessed/train_val_test_neg_paper_field.npz',
+             train_neg_paper_field=pf_neg_train,
+             val_neg_paper_field=pf_neg_val,
+             test_neg_paper_field=pf_neg_test)
+    np.savez('../data/preprocessed/train_val_test_pos_paper_field.npz',
+             train_pos_paper_field=pf_pos_train,
+             val_pos_paper_field=pf_pos_val,
+             test_pos_paper_field=pf_pos_test)
+
+
+
     # create dicts for metapath
+    # each dict has form: key: source - value: targets
     author_paper = defaultdict(list)
     paper_author = defaultdict(list)
     for a, p in data['author_paper']:
@@ -25,7 +61,7 @@ def find_meta_path(data):
         paper_author[p].append(a)
     author_field = defaultdict(list)
     field_author = defaultdict(list)
-    for a, f in data['author_field']:
+    for a, f in af_pos_train:
         author_field[a].append(f)
         field_author[f].append(a)
     child_parent_field = defaultdict(list)
@@ -35,43 +71,21 @@ def find_meta_path(data):
         parent_child_field[p].append(c)
     paper_field = defaultdict(list)
     field_paper = defaultdict(list)
-    for p, f in data['paper_field']:
+    for p, f in pf_pos_train:
         paper_field[p].append(f)
         field_paper[f].append(p)
 
-    # create metapaths
-    p_a_p = []
-    for a, p_list in tqdm.tqdm(author_paper.items(), desc='Meta-path p-a-p'):
-        p_a_p.extend([(p1, a, p2) for p1 in p_list for p2 in p_list])
-    p_a_p = np.array(p_a_p)
-    p_a_p[:, 1] += num_paper
 
-    p_f_p = []
-    for f, p_list in tqdm.tqdm(field_paper.items(), desc='Meta-path p-f-f'):
-        p_f_p.extend([(p1, f, p2) for p1 in p_list for p2 in p_list])
-    p_f_p = np.array(p_f_p)
-    p_f_p[:, 1] += num_paper + num_author
-
-    a_p_a = []
-    for p, a_list in tqdm.tqdm(paper_author.items(), desc='Meta-path a-p-a'):
-        a_p_a.extend([(a1, p, a2) for a1 in a_list for a2 in a_list])
-    a_p_a = np.array(a_p_a)
-    a_p_a[:, [0, 2]] += num_paper
-
-    a_f_a = []
-    for f, a_list in tqdm.tqdm(field_author.items(), desc='Meta-path a-f-a'):
-        a_f_a.extend([(a1, f, a2) for a1 in a_list for a2 in a_list])
-    a_f_a = np.array(a_f_a)
-    a_f_a += num_paper
-    a_f_a[:, 1] += num_author
-
-    f_f_f = []
-    for f, f_list in tqdm.tqdm(parent_child_field.items(), desc='Meta-path f-fp-f'):
-        f_f_f.extend([(f1, f, f2) for f1 in f_list for f2 in f_list])
-    for f, f_list in tqdm.tqdm(child_parent_field.items(), desc='Meta-path f-fc-f'):
-        f_f_f.extend([(f1, f, f2) for f1 in f_list for f2 in f_list])
-    f_f_f = np.array(f_f_f)
-    f_f_f += num_paper + num_author
+    p_a_p, p_f_p, a_p_a, a_f_a, f_f_f, p_f_a_f_p, a_f_p_f_a = get_meta_paths(author_paper,
+                                                                             paper_author,
+                                                                             author_field,
+                                                                             field_author,
+                                                                             child_parent_field,
+                                                                             parent_child_field,
+                                                                             paper_field,
+                                                                             field_paper,
+                                                                             num_author,
+                                                                             num_paper)
 
     # save metapaths
     expected_metapaths = [
@@ -113,3 +127,87 @@ def find_meta_path(data):
                     else:
                         out_file.write('{}\n'.format(target_idx))
                     left = right
+
+
+def get_meta_paths(author_paper, paper_author, author_field, field_author,
+                   child_parent_field, parent_child_field, paper_field, field_paper, num_author, num_paper):
+    # create metapaths
+    p_a_p = []
+    for a, p_list in tqdm.tqdm(author_paper.items(), desc='Meta-path p-a-p'):
+        p_a_p.extend([(p1, a, p2) for p1 in p_list for p2 in p_list])
+    p_a_p = np.array(p_a_p)
+    p_a_p[:, 1] += num_paper
+
+    p_f_p = []
+    for f, p_list in tqdm.tqdm(field_paper.items(), desc='Meta-path p-f-f'):
+        p_f_p.extend([(p1, f, p2) for p1 in p_list for p2 in p_list])
+    p_f_p = np.array(p_f_p)
+    p_f_p[:, 1] += num_paper + num_author
+
+    a_p_a = []
+    for p, a_list in tqdm.tqdm(paper_author.items(), desc='Meta-path a-p-a'):
+        a_p_a.extend([(a1, p, a2) for a1 in a_list for a2 in a_list])
+    a_p_a = np.array(a_p_a)
+    a_p_a[:, [0, 2]] += num_paper
+
+    a_f_a = []
+    for f, a_list in tqdm.tqdm(field_author.items(), desc='Meta-path a-f-a'):
+        a_f_a.extend([(a1, f, a2) for a1 in a_list for a2 in a_list])
+    a_f_a = np.array(a_f_a)
+    a_f_a += num_paper
+    a_f_a[:, 1] += num_author
+
+    f_f_f = []  # both FcFpFc and FpFcFp
+    for f, f_list in tqdm.tqdm(parent_child_field.items(), desc='Meta-path f-fp-f'):
+        f_f_f.extend([(f1, f, f2) for f1 in f_list for f2 in f_list])
+    for f, f_list in tqdm.tqdm(child_parent_field.items(), desc='Meta-path f-fc-f'):
+        f_f_f.extend([(f1, f, f2) for f1 in f_list for f2 in f_list])
+    f_f_f = np.array(f_f_f)
+    f_f_f += num_paper + num_author
+
+    p_f_a_f_p = []
+    # first obtain FAF
+    f_a_f = []
+    for a, f_list in tqdm.tqdm(author_field.items(), des="Meta-path f-a-f"):
+        f_a_f.extend([(f1, a, f2) for f1 in f_list for f2 in f_list])
+    # f_a_f = np.array(f_a_f)
+    # f_a_f += num_paper
+    # f_a_f[:, [0, 2]] += num_author
+    # next extends to two sides for PFAFP
+    for f1, a, f2 in f_a_f:
+        # check if these fields are connected to any author
+        if not field_paper[f1] or not field_paper[f2]:
+            continue
+        p1_candidate_index = np.random.choice(len(field_paper[f1]), math.ceil(0.2 * len(field_paper[f1])),
+                                              replace=False)
+        p1_candidate = field_paper[f1][p1_candidate_index]
+        p2_candidate_index = np.random.choice(len(field_paper[f2]), math.ceil(0.2 * len(field_paper[f2])),
+                                              replace=False)
+        p2_candidate = field_paper[f2][p2_candidate_index]
+        p_f_a_f_p.extend([(p1, f1, a, f2, p2) for p1 in p1_candidate for p2 in p2_candidate])
+    p_f_a_f_p = np.array(p_f_a_f_p)
+    p_f_a_f_p[:, [1, 2, 3]] += num_paper
+    p_f_a_f_p[:, [1, 3]] += num_author
+
+    a_f_p_f_a = []
+    # first obtain f_p_f
+    f_p_f = []
+    for p, f_list in tqdm.tqdm(paper_field.items(), des="Meta-path f-a-f"):
+        f_p_f.extend([(f1, p, f2) for f1 in f_list for f2 in f_list])
+    # f_p_f = np.array(f_p_f)
+    for f1, p, f2 in f_p_f:
+        # check if these fields are connected to any author
+        if not field_author[f1] or not field_author[f2]:
+            continue
+        a1_candidate_index = np.random.choice(len(field_author[f1]), math.ceil(0.2 * len(field_author[f1])),
+                                              replace=False)
+        a1_candidate = field_author[f1][a1_candidate_index]
+        a2_candidate_index = np.random.choice(len(field_author[f2]), math.ceil(0.2 * len(field_author[f2])),
+                                              replace=False)
+        a2_candidate = field_author[f2][a2_candidate_index]
+        a_f_p_f_a.extend([(a1, f1, p, f2, a2) for a1 in a1_candidate for a2 in a2_candidate])
+    a_f_p_f_a = np.array(a_f_p_f_a)
+    a_f_p_f_a[:, [0, 1, 3, 4]] += num_paper
+    a_f_p_f_a[:, [1, 3]] += num_author
+
+    return p_a_p, p_f_p, a_p_a, a_f_a, f_f_f, p_f_a_f_p, a_f_p_f_a
---
+++ split_and_sample.py
@@ -0,0 +1,41 @@
+from sklearn.model_selection import train_test_split
+'''
+def find_ap_path(author_field, field_paper):
+    """
+    find A-P pairs connected by either (1) a direct edge or (2) a AFP path.
+    return: two lists containing AP/AFP nodes
+    """
+    # all AFP paths
+    ap_dct = defaultdict(list)
+    a_f_p = []
+    a_f = []
+    for a, f_list in tqdm.tqdm(author_field.items(), desc='edge A-F'):
+        a_f.extend([(a, f) for f in f_list])
+    for a, f in a_f:
+        if not field_paper[f]:
+            continue
+        a_f_p.extend([(a, f, p) for p in field_paper[f]])
+        ap_dct[a].append(p)
+    a_f_p = np.array(a_f_p)
+    # a_f_p[:, [0, 1]] += num_paper
+    # a_f_p[:, 1] += num_author
+
+    return a_f_p, ap_dct
+'''
+
+def get_train_val_test_split(target, val_split, test_split):
+    train_target, mid_target = train_test_split(target, test_size=val_split + test_split)
+    val_target, test_target = train_test_split(mid_target, test_size=test_split / (val_split + test_split))
+
+    return train_target, val_target, test_target
+
+
+def get_neg_samples(target, dim_1, dim_2):
+    full = set()
+    for i in range(dim_1):
+        for j in range(dim_2):
+            full.add((i, j))
+    neg_set = full - set(map(tuple, target.tolist()))
+
+    return list(map(list, list(neg_set)))
+
